4762ddb83e6b6c11a25e4b384d61d7782669401a
bumjoooon <87797335+bumjoooon@users.noreply.github.com>
2023-10-12 00:58:57 +0900

    주석 처리된 부분 정리(삭제), 공백 줄이기

--- seminar_class_classes.sv
+++ seminar_class_classes.sv
@@ -1,25 +1,21 @@
 //hi this is branch : study_bjkim
-//`include "testbench.sv"
 //nametest : bumm
 //emailtest : bumm@conextt.com
 
-//Class 선언, 객체 생성, 멤버 변수 및 함수 정의
-
-
 class Total_Info;
-   static local int total_character_num;
-   static local int total_character_level;
+   static local int total_character_num;         //총 캐릭터의 수
+   static local int total_character_level;      //캐릭터 레벨의 총 합
 
-   virtual function total_new_character();
-      total_character_num++;
-      total_character_level++;
+   virtual function total_new_character();      //새 캐릭터 생성 시 
+      total_character_num++;                     //총 캐릭터 수 +1
+      total_character_level++;                  //레벨 총 합 +1
    endfunction
 
-   function total_character_levelup();
-      total_character_level += 1;
+   function total_character_levelup();         //캐릭터 레벨 업 시
+      total_character_level += 1;               //레벨 총 합 +1
    endfunction
 
-   function show_total_info();
+   function show_total_info();                  //총 정보 출력
      $display("total number : %d\n", total_character_num); 
      $display("total level : %d\n", total_character_level);
    endfunction
@@ -29,42 +25,26 @@ endclass
 
 
 class Beginner;
-   string job;
-   int name; 				// 캐릭터 이름
-   int level;
+   string job;                //직업 명
+   int name; 				      // 캐릭터 명(숫자로 표현)
+   int level;                 //특정 캐릭터의 레벨
    rand int STR;					//local int STR;	levelup 시 불가능 
    rand protected int DEX;		//자식 class에서 변경 가능
    rand int INT;					//보안화x : module에서 접근이 가능해서 문제 발생 가능
-   rand int LUK;				//rand : 난수를 받을 수 있는 변수 생성
-   //static int total_num;   //총 캐릭터 수
-   //static int total_level;  //모든 캐릭터들의 레벨 합산   
+   rand int LUK;				   //rand : 난수를 받을 수 있는 변수 생성
 
-  constraint c_LUK { STR inside {[1:10]}; DEX inside {[1:10]}; INT inside {[1:10]}; LUK inside {[1:10]}; }	//LUK 랜덤값 최대 최소
+   constraint c_LUK { STR inside {[1:10]}; DEX inside {[1:10]}; INT inside {[1:10]}; LUK inside {[1:10]}; }	//랜덤값 최대 최소
 
-  Total_Info to_in;
+   Total_Info to_in;         //Total_Info class 
   
-  
-   
   function new(string job,string name);
       this.job = job;
       this.name = name;
       this.level = 1;
       this.to_in = new();
-      //create_charactor();
-      
-      //total_num++;
-      //total_level++;
-        
-    
+
       this.to_in.total_new_character();
    endfunction
-
-//   function void create_charactor(int STR, int DEX, int INT);
-//      this.STR = STR;
-//      this.DEX = DEX;
-//      this.INT = INT;
-//      //this.LUK = LUK; 
-//   endfunction
 
    virtual function void attack();
       $display("%s basic attack !\n", name);
