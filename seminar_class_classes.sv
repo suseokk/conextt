d55074bd29becb7b54bba933721034b9934ab69f
sskang <sskang@conextt.com>
2023-10-12 17:11:39 +0900

    edit module, classes - warrior+100, mage+100

--- seminar_class_classes.sv
+++ seminar_class_classes.sv
@@ -1,33 +1,37 @@
 //Class 선언, 객체 생성, 멤버 변수 및 함수 정의
 class Beginner;
    string name;            // 캐릭터 이름
-   int level, STR, DEX, INT;
-   rand int LUK;           //rand : 난수를 받을 수 있는 변수 생성
-   static int total_num_c;   //총 캐릭터 수
+   //rand : 난수를 받을 수 있는 변수 생성
+   int level;
+   rand int STR, DEX, INT, LUK;
+   static int inst_cnt;   //총 캐릭터 수
    static int total_level; //모든 캐릭터들의 레벨 합산
+   int id;                 //character ID
 
-   constraint c_LUK { LUK inside {[1:10]}; } //LUK 랜덤값 최대 최소
+   constraint c_STAT { STR inside {[4:10]}; DEX inside {[4:10]}; INT inside {[4:10]}; LUK inside {[4:10]};} //LUK 랜덤값 최대 최소
 
-   function new(string name, int STR, int DEX, int INT);
+   function new(string name);
       this.name = name;
       this.level = 1;
-      this.STR = STR;
-      this.DEX = DEX;
-      this.INT = INT;
-      total_num_c++;
+      inst_cnt++;
       total_level++;
+      id = inst_cnt;
    endfunction
 
    virtual function void attack();
-      $display("%s basic attack !\n", name);
+      $display("%s(%0d) basic attack !\n", name,id);
+   endfunction
+
+   virtual function void skill();
+      $display("%s(%0d) has no skill\n", name,id);
    endfunction
 
    virtual function void stat();
-      $display("%s's STAT\nlevel : %0d\nSTR  : %0d\nDEX  : %0d\nINT  : %0d\nLUK  : %0d\n",name,level,STR,DEX,INT,LUK);
+      $display("%s(%0d)'s STAT\nlevel : %0d\nSTR  : %0d\nDEX  : %0d\nINT  : %0d\nLUK  : %0d\n",name,id,level,STR,DEX,INT,LUK);
    endfunction
 
    virtual function void levelup();
-      $display("%s LEVEL UP!!\n", name); // 공격 방식 변경 
+      $display("%s(%0d) LEVEL UP!!\n", name,id); // 공격 방식 변경 
       level = level + 1;
       STR = STR + 1;
       DEX = DEX + 1;
@@ -37,7 +41,7 @@ class Beginner;
    endfunction
 
    function void Total_Info();
-      $display("total number of characters: %0d", total_num_c); 
+      $display("total number of characters: %0d", inst_cnt); 
       $display("total level : %0d\n", total_level);
    endfunction
 endclass
@@ -45,28 +49,28 @@ endclass
 
 //상속(Inheritance)
 class Job extends Beginner;
-   function new(string name, int STR, int DEX, int INT);
-      super.new(name,STR, DEX, INT); // 부모 클래스인 Beginner의 생성자 호출 
+   function new(string name);
+      super.new(name); // 부모 클래스인 Beginner의 생성자 호출 
    endfunction
 
    //다형성(Polymorphism)
    virtual function void attack();
       if(name == "Warrior")
-         $display("%s Sword Attack!\n", name); // 공격 방식 변경 
-      if(name == "Mage")
-         $display("%s Swing Wand!\n", name); // 공격 방식 변경 
+         $display("%s(%0d) Sword Attack!\n", name,id); // 공격 방식 변경 
+      else if(name == "Mage")
+         $display("%s(%0d) Swing Wand!\n", name,id); // 공격 방식 변경 
    endfunction
 
    virtual function void skill();
       if(name == "Mage")
-         $display("%s Energy bolt!\n", name); // Mage 스킬
+         $display("%s(%0d) Energy bolt!\n", name,id); // Mage 스킬
    endfunction 
 
    virtual function void levelup();
       super.levelup();
       if(name == "Warrior")
          STR = STR + 3;
-      if(name == "Mage")
+      else if(name == "Mage")
          INT = INT + 3;
    endfunction
 endclass
--- seminar_class_module.sv
+++ seminar_class_module.sv
@@ -1,49 +1,39 @@
 module Game;
 Beginner b0;
 Job j0;
+Beginner user[$];
 
 initial begin
-   $display("\nSTART\n");
-   b0 = new("Player",9,8,4);
-   b0.randomize; 
-   b0.stat();
-   b0.attack(); 
-   b0.STR = b0.STR +1;
-   b0.stat();
-
-   for (int i = 0; i < 2; i++) begin
-      if(i == 0)
-         j0 = new("Warrior",9,4,8);
-      else
-         j0 = new("Warrior",11,4,7);
-      j0.randomize; 
-      j0.stat();
-      j0.attack();
-      j0.levelup();
-      j0.stat();
-      j0.Total_Info();
+   //Set Characters
+   //Beginner
+   for (int i = 0; i < 1; i++) begin
+      b0 = new("Beginner");
+      b0.randomize;
+      user.push_back(b0);
    end
-
-
-   b0 = j0;                     //b0에 j0 
-   b0.attack();
-
-
-   for (int i = 0; i < 3; i++) begin
-      if(i == 0)
-         j0 = new("Mage",4,4,10);
-      else if(i == 1)
-         j0 = new("Mage",4,6,10);
-      else
-         j0 = new("Mage",6,4,11);
+   //Warrior
+   for (int i = 0; i < 102; i++) begin
+      j0 = new("Warrior");
+      j0.randomize();
+      user.push_back(j0);
+   end
+   //Mage
+   for (int i = 0; i < 103; i++) begin
+      j0 = new("Mage");
       j0.randomize;
-      j0.stat();
-      j0.attack();
-      j0.skill();
-      j0.levelup();
-      j0.stat();
-      j0.Total_Info();
+      user.push_back(j0);
    end
-   $finish; 
+
+   //Act Characters
+   $display("\nSTART\n");
+   for (int i = 0; i < user.size(); i++) begin
+      user[i].stat();
+      user[i].attack();
+      user[i].skill();
+      user[i].levelup();
+      user[i].stat();
+      user[i].Total_Info();
+   end
+   $finish;
 end
 endmodule
