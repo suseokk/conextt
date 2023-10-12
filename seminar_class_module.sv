61befe08607b979b2d92548e4f4d34019ccd1420
bumjoooon <87797335+bumjoooon@users.noreply.github.com>
2023-10-12 19:42:30 +0900

    beginner 10레벨 까지 올리기 추가, beginner 레벨 10 도달 시 전직 추가

--- seminar_class_module.sv
+++ seminar_class_module.sv
@@ -1,17 +1,25 @@
 module Game;
   
+  Beginner b0;
+  
   Warrior w0;
   Mage m0;
 
   Beginner my_party[$];
 
 initial begin
+  
+  for (int i = 0 ; i < 2 ; i++) begin
+    b0 = new();
+    my_party.push_back(b0); // warrior
+   end
+  
 
-  for (int i = 0 ; i < 3 ; i++) begin
+/*  for (int i = 0 ; i < 3 ; i++) begin
      w0 = new();
      my_party.push_back(w0); // warrior
    end
-
+*/
   for (int i = 0 ; i < 5 ; i++) begin
       m0 = new;
      my_party.push_back(m0); // mage
@@ -26,14 +34,37 @@ initial begin
      my_party[i].attack();
    end
 
-   for (int i = 0; i< my_party.size(); i++) begin
-     my_party[i].levelup();
-   end
+  
+  foreach(my_party[i])begin
+    if(my_party[i].job=="Beginner")begin
+      for(int a=1;a<10;a++)
+        my_party[i].levelup();
+    end
+  end
+  
+  
+  foreach(my_party[i])begin
+    if(my_party[i].job=="Beginner" && my_party[i].level>9)begin
+      if(my_party[i].STR > my_party[i].INT) begin
+        w0 = new();
+        my_party.push_back(w0); // warrior
+        my_party[$].nickname = my_party[i].nickname;
+        my_party[$].STR = my_party[i].STR;
+        my_party[$].DEX = my_party[i].DEX;
+        my_party[$].INT = my_party[i].INT;
+        my_party[$].LUK = my_party[i].LUK;
+        my_party[$].stat();
+      end
+
+    end
+  end
+  
+ 
   
   
   my_party[1].show_total_info();
   
-  foreach(my_party[i].level)
+  //foreach(my_party[i].level);
 
 
 end
