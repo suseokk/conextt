da979999b60e96bf6cc3e2a8376526e3185dadf8
bumjoooon <87797335+bumjoooon@users.noreply.github.com>
2023-10-12 17:07:03 +0900

    초기버전

--- seminar_class_classes.sv
+++ seminar_class_classes.sv
@@ -2,107 +2,105 @@
 //`include "testbench.sv"
 
 //Class 선언, 객체 생성, 멤버 변수 및 함수 정의
-class character;
-   string name;
-   string job;// 캐릭터 이름
+class Beginner;
+   string name; 				// 캐릭터 이름
    int level;
    int STR;					//local int STR;	levelup 시 불가능 
-   int DEX;		//자식 class에서 변경 가능
+   protected int DEX;		//자식 class에서 변경 가능
    int INT;					//보안화x : module에서 접근이 가능해서 문제 발생 가능
-   int LUK;				//rand : 난수를 받을 수 있는 변수 생성
+   rand int LUK;				//rand : 난수를 받을 수 있는 변수 생성
    static int total_num;   //총 캐릭터 수
    static int total_level;  //모든 캐릭터들의 레벨 합산
    
 
-   function new(string name);
+   constraint c_LUK { LUK inside {[1:10]}; }	//LUK 랜덤값 최대 최소
+
+   function new(string name, int STR, int DEX, int INT);
       this.name = name;
       this.level = 1;
-      this.STR = 5;
-      this.DEX = 5;
-      this.INT = 5;
-      this.LUK = 5;
+      create_charactor(STR, DEX, INT);
+      
       total_num++;
       total_level++;
-      this.set_job;
    endfunction
 
-   task show_status;
-      $display("%s[%s] : LEVEL[%0d] STR[%0d] DEX[%0d] INT[%0d] LUK[%0d]",
-                  name, job, level, STR, DEX, INT, LUK );
-
-   virtual task set_job;
-      job = "beginner";
-   endtask
-
-   virtual task attack;
-      $display("%s[%s] : <%d>damage basic attack!! ", name, job, STR*10);
-   endtask
-
-   virtual function LEVEL_UP;
-      this.level= this.level + 1;
-      total_level++;
-      $display("%s[%s] : LEVEL UP!!! [lv.%0d] => [lv.%0d]", name, job, level-1, level);
-      this.STR = this.STR + 3;
-      this.DEX = this.DEX + 3;
-      this.INT = this.INT + 3;
-      this.LUK = this.LUK + 3;
+   function void create_charactor(int STR, int DEX, int INT);
+      this.STR = STR;
+      this.DEX = DEX;
+      this.INT = INT;
+      //this.LUK = LUK; 
    endfunction
+
+   virtual function void attack();
+      $display("%s basic attack !\n", name);
+   endfunction
+
+   function void stat();
+      $display("%s's STAT \nlevel:%d\nSTR:%d\nDEX:%d\nINT:%d\nLUK:%d\n",name,level,STR,DEX,INT,LUK);
+   endfunction
+
+   function levelup();
+      $display("%s LEVEL UP!!\n", name); // 공격 방식 변경 
+      level = level + 1;
+      STR = STR + 1;
+      DEX = DEX + 1;
+      INT = INT + 1;
+      LUK = LUK + 1;
+      
+      total_level += 1; // 전체 레벨 업데이트
+   endfunction
+
+   function Total_Info();
+      $display("total number : %d\n", total_num); 
+      $display("total level : %d\n", total_level);
+      
+   endfunction
+
 endclass
 
 
-/////////////////////////////////////
-///////////////child/////////////////
-/////////////////////////////////////
+//상속(Inheritance)
+class Warrior extends Beginner;
 
-class Warrior extends character;
-   function new(string name);
-      super.new(name);
+   function new(string name, int STR, int DEX, int INT);
+      super.new(name,STR, DEX, INT); // 부모 클래스인 Beginner의 생성자 호출 
    endfunction
 
-   virtual task set_job;
-      job = "Worrior";
-   endtask
-
-   virtual task attack;
-      $display("%s[%s] : <%d>damage Big Sword attack!! ", name, job, STR*17);
-   endtask
-
-   virtual function LEVEL_UP;
-      super.LEVEL_UP;
-      this.STR = this.STR + 5;
+   //다형성(Polymorphism)
+   virtual function void attack();
+      $display("%s Sword Attack!\n", name); // 공격 방식 변경 
    endfunction
+
+   function levelup();
+      $display("%s LEVEL UP!!\n", name); // 공격 방식 변경 
+      STR = STR + 3;
+      DEX = DEX + 2;
+      INT = INT + 1;
+      LUK = LUK + 1;
+   endfunction
+
+
 endclass
 
+class Mage extends Beginner;
 
-class Mage extends character;
-   function new(string name);
-      super.new(name);
+   function new(string name, int STR, int DEX, int INT);
+      super.new(name,STR, DEX, INT); 
    endfunction
 
-   virtual task set_job;
-      job = "Mage";
-   endtask
-
-   virtual task attack;
-      $display("%s[%s] : <%d>damage Magic fire attack!! ", name, job, INT*17);
-   endtask
-
-   virtual function LEVEL_UP;
-      super.LEVEL_UP;
-      this.INT = this.INT + 5;
+   virtual function void attack();
+      $display("%s Swing Wand!\n", name); // 공격 방식 변경 
    endfunction
+
+   function void mage_skill();
+      $display("%s Energy bolt!\n", name); // Mage 스킬
+   endfunction 
+
+   function levelup();
+      super.levelup();
+
+      INT = INT + 2;
+      LUK = LUK + 1;
+   endfunction
+
 endclass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
