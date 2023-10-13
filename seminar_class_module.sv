88a320db4017fc1448722d3114cc6fddb7ae244c
bumjoooon <87797335+bumjoooon@users.noreply.github.com>
2023-10-13 10:31:59 +0900

    전사 레벨업 테스트 코드 작성

--- seminar_class_module.sv
+++ seminar_class_module.sv
@@ -7,11 +7,15 @@ module Game;
 
   Beginner my_party[$];
 
+	Total_Info tot_inf;
+
 initial begin
   
   for (int i = 0 ; i < 2 ; i++) begin
     b0 = new();
+    tot_inf = new();
     my_party.push_back(b0); // warrior
+    tot_inf.total_new_character();
    end
   
 
@@ -49,31 +53,44 @@ initial begin
         w0 = new();
         my_party.push_back(w0); // warrior 전직
         my_party[$].nickname = my_party[i].nickname;
+        my_party[$].level = my_party[i].level;
         my_party[$].STR = my_party[i].STR;
         my_party[$].DEX = my_party[i].DEX;
         my_party[$].INT = my_party[i].INT;
         my_party[$].LUK = my_party[i].LUK;
         my_party[$].stat();
+        //my_party[i]=null;
+        //my_party[i].stat();
       end
       
       else begin
         m0 = new();
         my_party.push_back(m0); // mage 전직
         my_party[$].nickname = my_party[i].nickname;
+        my_party[$].level = my_party[i].level;
         my_party[$].STR = my_party[i].STR;
         my_party[$].DEX = my_party[i].DEX;
         my_party[$].INT = my_party[i].INT;
         my_party[$].LUK = my_party[i].LUK;
         my_party[$].stat();
+        //my_party[i]=null;
+        //my_party[i].stat();
       end
 
     end
   end
   
+  
+  foreach(my_party[i])begin
+    if(my_party[i].job=="Warrior")begin
+      for(int a=1;a<3;a++)
+        my_party[i].levelup();
+    end
+  end
  
   
   
-  my_party[1].show_total_info();
+  my_party[3].show_total_info();
   
   //foreach(my_party[i].level);
 
@@ -82,3 +99,5 @@ end
 
 
 endmodule
+
+
