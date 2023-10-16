ec7b13d32d4938209e1453f7db8b60e6d3afef0c
Hannah Lee <hnlee@conextt.com>
2023-10-16 09:07:24 +0900

    message

--- seminar_class_classes.sv
+++ seminar_class_classes.sv
@@ -1,87 +1,73 @@
-//Class 선언, 객체 생성, 멤버 변수 및 함수 정의
 class Beginner;
-   string name; 				// 캐릭터 이름
-   string weapon;
-   int level;
-   int STR;					//local int STR;	levelup 시 불가능 
-   protected int DEX;		//자식 class에서 변경 가능
-   int INT;					//보안화x : module에서 접근이 가능해서 문제 발생 가능
-   rand int LUK;				//rand : 난수를 받을 수 있는 변수 생성
+   string job; 
+	int level;
+	int STR, DEX, INT;					//local int STR;	levelup 시 불가능 
+	rand int LUK; 				//rand : 난수를 받을 수 있는 변수 생성
   
-   static int total_num;   //총 캐릭터 수
-   int id;
-//   static int total_level;  //모든 캐릭터들의 레벨 합산
+	static int total_num;   //총 캐릭터 수
+	int id;
+	//static int total_level;  //모든 캐릭터들의 레벨 합산
    
-
-   constraint c_LUK { LUK inside {[1:10]}; }	//LUK 랜덤값 최대 최소
-  
+	constraint c_LUK { LUK inside {[1:10]}; }	//LUK 랜덤값 최대 최소
 	
-  function new(string name, string weapon, int STR, int DEX, int INT);
-      this.name = name;
-      this.weapon = weapon;
-      this.level = 1;
-      create_charactor(STR, DEX, INT);
+	function new;
+		job = "Beginner";
+		weapon = "Basic attack !\n";
+
+		level = 1;
+		STR = 1;
+		DEX = 1;
+		INT = 1;
+		id = total_num++;
+    		//total_level++;
+	endfunction
+
+	virtual function void attack();
+		$display("%s(%0d)'s %s", job, id, weapon);
+	endfunction
+
+	function void stat();
+		$display("%s(%0d)'s STAT \nlevel:\t%d\nSTR:\t%d\nDEX:\t%d\nINT:\t%d\nLUK:\t%d\n",job,id,level,STR,DEX,INT,LUK);
+	endfunction
+
+	virtual function levelup();
+		$display("%s(%0d) LEVEL UP!!\n", job, id);
+		level = level + 1;
+		STR = STR + 1;
+		DEX = DEX + 1;
+		INT = INT + 1;
+		LUK = LUK + 1;
       
-      total_num++;
-      //total_level++;
-   endfunction
-
-   function void create_charactor(int STR, int DEX, int INT);
-      this.STR = STR;
-      this.DEX = DEX;
-      this.INT = INT;
-      //this.LUK = LUK; 
-   endfunction
-
-   function void attack();
-     $display("%s %s attack !\n", name, weapon);
-   endfunction
-
-   function void stat();
-     $display("%s's STAT \nlevel:\t%d\nSTR:\t%d\nDEX:\t%d\nINT:\t%d\nLUK:\t%d\n",name,level,STR,DEX,INT,LUK);
-   endfunction
-
-   virtual function levelup();
-     $display("%s LEVEL UP!!\n", name); // 공격 방식 변경 
-      level = level + 1;
-      STR = STR + 1;
-      DEX = DEX + 1;
-      INT = INT + 1;
-      LUK = LUK + 1;
-      
- //     total_level += 1; // 전체 레벨 업데이트
-   endfunction
-
-
+ 		//total_level += 1; // 전체 레벨 업데이트
+	endfunction
 endclass
-
 
 //상속(Inheritance)
 class Warrior extends Beginner;
 
-  function new(string name, string weapon, int STR, int DEX, int INT);
-    super.new(name, weapon, STR, DEX, INT); // 부모 클래스인 Beginner의 생성자 호출 
-   endfunction
+	function new;
+		super.new; 
+		this.job = "Warrior";
+		this.weapon = "Sword attack !\n";
+	endfunction
 
-   virtual function levelup();
-     super.levelup();
-      STR = STR + 2;
-      DEX = DEX + 1;
-   endfunction
-
-
+	virtual function levelup();
+		super.levelup();
+		STR = STR + 2;
+		DEX = DEX + 1;
+	endfunction
 endclass
 
 class Mage extends Beginner;
+	function new;
+		super.new; 
+		this.job = "Mage";
+		this.weapon = "Wand attack !\n";
+	endfunction
 
-  function new(string name, string weapon, int STR, int DEX, int INT);
-     super.new(name, weapon, STR, DEX, INT); 
-   endfunction
-
-   virtual function levelup();
-      super.levelup();
-      INT = INT + 2;
-      LUK = LUK + 1;
-   endfunction
-
+	virtual function levelup();
+		super.levelup();
+		INT = INT + 2;
+		LUK = LUK + 1;
+	endfunction
 endclass
--- seminar_class_module.sv
+++ seminar_class_module.sv
@@ -1,45 +1,44 @@
 module Game;
-  
-Beginner my_party[$];
-  
-Warrior w1;
-Mage m1;
-  
-int num_Warrior = 2;  
-int num_Mage = 3;
-  
-initial begin
-  
-  for(int i=0; i<num_Warrior; i++) begin
-    w1 = new("Warrior","Sword",1,1,1);
-    w1.randomize();
-    my_party.push_back(w1);
-  end
+	Beginner my_party[$];
+	Warrior w1;
+	Mage m1;
+	
+	//int num_Warrior = 102;  
+	int num_Warrior = 2;  
+	//int num_Mage = 103;
+	int num_Mage = 3;
 
-  for(int i=0; i<num_Mage; i++) begin
-    m1 = new("Mage","Wand",1,1,1);
-    m1.randomize();
-    my_party.push_back(m1);
-  end
-  
-  for(int i=0; i<my_party.size(); i++) begin
-    my_party[i].attack();
-  end
-  
-  for(int i=0; i<my_party.size(); i++) begin
-    my_party[i].stat();
-  end
+	initial begin
+		t1 = new;
+		
+		for(int i=0; i<num_Warrior; i++) begin
+			w1 = new;
+			w1.randomize();
+			my_party.push_back(w1);
+		end
+		
+		for(int i=0; i<num_Mage; i++) begin
+			m1 = new;
+			m1.randomize();
+			my_party.push_back(m1);
+	    	end
 
-  for(int i=0; i<my_party.size(); i++) begin
-    my_party[i].levelup();
-  end
-  
-  for(int i=0; i<my_party.size(); i++) begin
-    my_party[i].stat();
-  end
+		for(int i=0; i<my_party.size(); i++) begin
+			my_party[i].stat();
+		end
 
-   $finish; 
-
-end
-
+		for(int i=0; i<my_party.size(); i++) begin
+			my_party[i].attack();
+		end
+		
+		for(int i=0; i<my_party.size(); i++) begin
+			my_party[i].levelup();
+		end
+	
+		for(int i=0; i<my_party.size(); i++) begin
+			my_party[i].stat();
+		end
+		
+		$finish; 
+	end
 endmodule
