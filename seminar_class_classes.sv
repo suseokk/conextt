1fca0fc169f7bc803ed376070fe50c0f50cf4c18
Hannah Lee <hnlee@conextt.com>
2023-10-16 09:32:49 +0900

    total info

--- seminar_class_classes.sv
+++ seminar_class_classes.sv
@@ -1,36 +1,50 @@
+class Total_info;
+   static int total_num;
+   static int total_level;
+
+   task show;
+      $display("Total Level is %0d\n", total_level);
+   endtask
+endclass
+
 class Beginner;
-   string job; 
+   string job, weapon, skill; 
 	int level;
 	int STR, DEX, INT;					//local int STR;	levelup 시 불가능 
-	rand int LUK; 				//rand : 난수를 받을 수 있는 변수 생성
-  
-	static int total_num;   //총 캐릭터 수
-	int id;
-	//static int total_level;  //모든 캐릭터들의 레벨 합산
+	rand int LUK; 				//rand : 난수를 받을 수 있는 변수 생성i
+  	int id;
    
 	constraint c_LUK { LUK inside {[1:10]}; }	//LUK 랜덤값 최대 최소
 	
+   Total_info Info;
+
+   task show_info;
+      Info.show;
+   endtask
+
 	function new;
 		job = "Beginner";
 		weapon = "Basic attack !\n";
-
+      skill = "has No skills !\n";
+      
 		level = 1;
 		STR = 1;
 		DEX = 1;
 		INT = 1;
-		id = total_num++;
-    		//total_level++;
+		id = Info.total_num++;
+    	Info.total_level++;
 	endfunction
 
-	virtual function void attack();
+   function void attack();
 		$display("%s(%0d)'s %s", job, id, weapon);
-	endfunction
+		$display("%s(%0d)'s %s", job, id, skill);
+   endfunction
 
 	function void stat();
 		$display("%s(%0d)'s STAT \nlevel:\t%d\nSTR:\t%d\nDEX:\t%d\nINT:\t%d\nLUK:\t%d\n",job,id,level,STR,DEX,INT,LUK);
 	endfunction
 
-	virtual function levelup();
+	virtual function void levelup();
 		$display("%s(%0d) LEVEL UP!!\n", job, id);
 		level = level + 1;
 		STR = STR + 1;
@@ -38,20 +52,20 @@ class Beginner;
 		INT = INT + 1;
 		LUK = LUK + 1;
       
- 		//total_level += 1; // 전체 레벨 업데이트
+ 		Info.total_level += 1; // 전체 레벨 업데이트
 	endfunction
 endclass
 
 //상속(Inheritance)
 class Warrior extends Beginner;
-
 	function new;
 		super.new; 
 		this.job = "Warrior";
 		this.weapon = "Sword attack !\n";
+      this.skill = "Slash blast !\n";
 	endfunction
 
-	virtual function levelup();
+	virtual function void levelup();
 		super.levelup();
 		STR = STR + 2;
 		DEX = DEX + 1;
@@ -63,9 +77,10 @@ class Mage extends Beginner;
 		super.new; 
 		this.job = "Mage";
 		this.weapon = "Wand attack !\n";
-	endfunction
+		this.skill = "Energy bolt !\n";
+   endfunction
 
-	virtual function levelup();
+	virtual function void levelup();
 		super.levelup();
 		INT = INT + 2;
 		LUK = LUK + 1;
--- seminar_class_module.sv
+++ seminar_class_module.sv
@@ -2,9 +2,11 @@ module Game;
 	Beginner my_party[$];
 	Warrior w1;
 	Mage m1;
+
+   Total_info t1;
 	
-	//int num_Warrior = 102;  
-	int num_Warrior = 2;  
+	//int num_Warrior = 102;
+	int num_Warrior = 2; 
 	//int num_Mage = 103;
 	int num_Mage = 3;
 
@@ -24,6 +26,10 @@ module Game;
 	    	end
 
 		for(int i=0; i<my_party.size(); i++) begin
+			my_party[i].Info = t1;
+		end
+
+		for(int i=0; i<my_party.size(); i++) begin
 			my_party[i].stat();
 		end
 
@@ -33,6 +39,7 @@ module Game;
 		
 		for(int i=0; i<my_party.size(); i++) begin
 			my_party[i].levelup();
+ 			my_party[i].show_info();
 		end
 	
 		for(int i=0; i<my_party.size(); i++) begin
